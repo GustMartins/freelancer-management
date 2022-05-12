408dd2a64b704e22c68f8acd1c48d6a028064ce9
Gustavo Martins <gustavo_martins92@hotmail.com>
2022-05-12 17:27:34 -0300

    Adicione jest para testes

---
+++ src/shared/helpers/__tests__/records.spec.ts
@@ -0,0 +1,25 @@
+import { createClient } from '@architect/shared/helpers/records'
+import { ClientEntity } from '@architect/shared/interfaces/records.types'
+
+describe('shared/helpers/records funções com registros', () => {
+  describe('createClient()', () => {
+    it('deveria gerar um registro de cliente', () => {
+      const id = 'id-do-cliente'
+      const email = 'cliente@email.com'
+      const document = '999.999.999-99'
+      const password = 'G5AL98RB'
+
+      const clientCreated = createClient(id, email, document, password)
+
+      expect(clientCreated).toMatchObject<ClientEntity>({
+        Pk: `C#${id}`,
+        Sk: 'Profile',
+        ListPk: 'Client',
+        Email: email,
+        Password: password,
+        Document: document,
+        DomainCount: 0
+      })
+    })
+  })
+})
