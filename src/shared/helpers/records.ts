d33aacd274b8a0dc34142e7d7fd91d17867018b4
Gustavo Martins <gustavo_martins92@hotmail.com>
2022-05-12 14:15:46 -0300

    Adicione tipagem dos dados e funções de criação

---
+++ src/shared/helpers/records.ts
@@ -0,0 +1,95 @@
+import {
+  ClientEntity, DomainEntity, LogEntity, LogEntityKinds, MetricEntity,
+  PaymentEntity, PaymentEntityStatuses
+} from '../interfaces/records.types'
+
+/**
+ * Função para criar um registro de usuário
+ * @param id Identificador do usuário
+ * @param email E-mail do usuário
+ * @param password Senha de acesso aos dados analíticos
+ * @returns Dados do registro do clientes
+ */
+export function createClient(id: string, email: string, document: string, password: string): ClientEntity {
+  return {
+    Pk: `C#${id}`,
+    Sk: 'Profile',
+    ListPk: 'Client',
+    Email: email,
+    Password: password,
+    Document: document,
+    DomainCount: 0
+  }
+}
+
+/**
+ * Função para criar um registro de pagamento
+ * @param id Identificador do usuário
+ * @param year Ano de validade do pagamento
+ * @param date Data da criação do registro
+ * @returns Dados do registro do pagamento
+ */
+export function createPayment(id: string, year: number, date: Date): PaymentEntity {
+  const status: PaymentEntityStatuses = 'created'
+
+  return {
+    Pk: `C#${id}`,
+    Sk: `Pay#${year}`,
+    ListPk: `Payment`,
+    StatusSk: `${status}#${date.toISOString()}`
+  }
+}
+
+/**
+ * Função para criar um registro de domínio
+ * @param client Registro do cliente
+ * @param id Identificador do domínio
+ * @param domain URL do domínio
+ * @returns Dados do registro do domínio
+ */
+export function createDomain(client: ClientEntity, id: string, domain: string): DomainEntity {
+  return {
+    Pk: client.Pk,
+    Sk: `D@${id}`,
+    ListPk: 'Domain', 
+    Client: client.Email, 
+    Website: domain
+  }
+}
+
+/**
+ * Função para criar um registro de métrica mensal
+ * @param domain Identificador do domínio
+ * @param date Data inicial do período das métricas
+ * @returns Dados do registro da métrica
+ */
+export function createMetric(domain: string, date: Date): MetricEntity {
+  return {
+    Pk: `D@${domain}`,
+    Sk: `Metrics@${date.toISOString().substring(0, 7)}`,
+    Content: {
+      Created: date.toISOString(),
+      SessionsCount: 0,
+      PagesCount: 0,
+      EventsCount: 0,
+      Pages: {},
+      Events: {}
+    }
+  }
+}
+
+/**
+ * Função para um registro de log de métrica
+ * @param domain Identificador do domínio
+ * @param type Tipo de registro de log
+ * @param date Data do registro
+ * @returns Dados do registro de um log de métrica
+ */
+export function createLog(domain: string, type: LogEntityKinds, date: Date, data: Record<string, any>): LogEntity {
+  return {
+    Pk: `D@${domain}`,
+    Sk: `${type}#${date.toISOString()}`,
+    Kind: type,
+    Content: data
+  }
+}
---
+++ src/shared/interfaces/records.types.ts
@@ -0,0 +1,81 @@
+// TODO: Verificar se é necessário documentar melhor as interfaces disponíveis
+
+/**
+ * Chaves de registro
+ * 
+ * Os registros no banco de dados possuem todos as propriedades Pk e Sk.
+ */
+export interface RecordHashKey {
+  Pk: string
+  Sk: string
+}
+
+/**
+ * Chave para listas
+ */
+export interface RecordListsGSIKey {
+  ListPk: string
+}
+
+/**
+ * Chave para pagamentos
+ */
+export interface RecordPaymentsGSIKey {
+  ListPk: string
+  StatusSk: string
+}
+
+/**
+ * Chave para localizar referências de pagamentos
+ */
+export interface RecordPicPayGSIKey {
+  PI: string
+}
+
+export interface ClientEntity extends RecordHashKey, RecordListsGSIKey {
+  ListPk: 'Client'
+  Email: string
+  Password: string
+  Document: string
+  DomainCount: number
+}
+
+/**
+ * Status disponível a um pagamento
+ * 
+ * Essa lista é baseada principalmente nos status de pagamento da plataforma
+ * PicPay utilizada para processar os pagamentos.
+ * Veja: https://studio.picpay.com/produtos/e-commerce/checkout/guides/order-status
+ */
+export type PaymentEntityStatuses = 
+    'created' 
+  | 'expired' 
+  | 'analysis' 
+  | 'paid' 
+  | 'completed' 
+  | 'refunded' 
+  | 'chargeback'
+
+export interface PaymentEntity extends RecordHashKey, RecordPaymentsGSIKey {
+  ListPk: 'Payment'
+}
+
+export interface DomainEntity extends RecordHashKey, RecordListsGSIKey {
+  ListPk: 'Domain'
+  Client: string
+  Website: string
+}
+
+export interface MetricEntity extends RecordHashKey {
+  Content: Record<string, any>
+}
+
+export type LogEntityKinds = 'Sessions' | 'Pages' | 'Events'
+
+export interface LogEntity extends RecordHashKey {
+  Kind: LogEntityKinds
+  Content: Record<string, any>
+}
+
+// TODO: Verificar se é necessário essa entidade
+export interface ArrearPaymentEntity extends RecordHashKey {}
