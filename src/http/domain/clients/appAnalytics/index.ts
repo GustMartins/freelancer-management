74be114a17a046515081486285954d3617b17882
Gustavo Martins <gustavo_martins92@hotmail.com>
2022-05-24 23:57:10 -0300

    Adicione o endpoint GET /c/analytics/:app

--- src/http/domain/clients/appAnalytics/index.ts
+++ src/http/domain/clients/appAnalytics/index.ts
@@ -1,11 +1,38 @@
 import { http } from '@architect/functions'
+import { PayloadError } from '@architect/shared/helpers/errors'
+import { send, sendError } from '@architect/shared/helpers/http'
 import {
   ApplicationRequest
 } from '@architect/shared/interfaces/application.types'
 import { auth } from '@architect/shared/middlewares/auth'
+import { client } from '@architect/shared/middlewares/client'
+import { listAnalytics } from '@architect/shared/repositories/domains'
 
-async function appAnalytics (request: ApplicationRequest): Promise<any> {
-  return request
+import validator from './input'
+
+async function appAnalyticsHandler (request: ApplicationRequest): Promise<any> {
+  try {
+    if (!validator(request.query)) {
+      throw new PayloadError('Propriedades query incorretas.')
+    }
+
+    const { year, start, end } = request.query || {}
+
+    const list = year
+      ? await listAnalytics(request.pathParameters.app, parseInt(year))
+      : start && end
+        ? await listAnalytics(request.pathParameters.app, parseInt(year), new Date(start), new Date(end))
+        : await listAnalytics(request.pathParameters.app)
+
+    return send({
+      body: list.map(metric => ({
+        period: metric.Sk.split('@').pop(),
+        data: metric.Content
+      }))
+    })
+  } catch (error) {
+    return sendError(error)
+  }
 }
 
-export const handler = http.async(auth, appAnalytics)
+export const handler = http.async(auth, client, appAnalyticsHandler)
